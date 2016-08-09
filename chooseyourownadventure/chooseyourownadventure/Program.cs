2c3c48b8caa79e8ee166a666c532659676226f63
Ginkobaloba <Dramattick1@gmail.com>
2016-08-09 14:19:17 -0500

    Choose your own adventure added classes

---
+++ chooseyourownadventure/chooseyourownadventure/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChooseAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            bool torchout = false;
+            bool sworddrawn = false;
+            //bool mapout = false;
+
+            Console.Write(" your inventory contains a Map, a sword, and a torch. You  approach the entrance of a dark cave how do you proceed? \n");
+            string decision = Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ chooseyourownadventure/chooseyourownadventure/chooseyourownfunctions.cs
@@ -0,0 +1,55 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ChooseAdventure
+//{
+//    class CreatePlayer(string PlayerName, bool sworddrawn, bool torchout)
+        
+
+
+
+//    class mouthofcave(string decision, bool sworddrawn, bool torchout)
+//    {
+//        switch (decision)
+//            {
+//                case "Enter Cave":
+//                    if (torchout == true)
+//                    {
+//                        Console.WriteLine("As you enter the cave your torch illuminates your path. You proceed forward until you reach a fork in the cave to the left you hear a low whine, to the right a constant steady pound how do you proceed?");
+//                    }
+//                    else if (sworddrawn == true)
+//                    {
+//                        Console.WriteLine("as you enter the darkened cave with your sword drawn. You are rushed by a necromancer's hord. You swing blindly hoping to find purchase. You are quickly overcome, the hord slowly consumes your body in the darkness. YOU DIE. ");
+//                    }
+//                    else
+//                    {
+//                        Console.WriteLine("As you enter the pitch black cave you hear a rush of commotion. You are swarmed by a necromancer's hord. YOU DIE. ");
+//                    }
+//                    break;
+//                case "Draw Sword":
+//                    if (torchout == true)
+//                    {
+//                        torchout = false;
+//                        Console.WriteLine("You put away your torch and Draw your sword.");
+//                    }
+//                    else if (sworddrawn == true)
+//                    {
+//                        Console.WriteLine("Your Sword is already drawn.");
+//                    }
+//                    else
+//                    {
+//                        Console.WriteLine("The sound of cold steel rings in the air as you draw your sword.");
+//                    }
+//                    break;
+//                    default:
+//                    {
+//                        Console.WriteLine("Not a valid Choice.");
+                        
+//                    }
+
+//            }
+//    }
+//}
