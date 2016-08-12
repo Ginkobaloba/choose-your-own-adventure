8b0c38854b6cf08af2131e06e2fa51b838ffc44b
Ginkobaloba <Dramattick1@gmail.com>
2016-08-12 14:25:46 -0500

    inital Commit

---
+++ DiceGame/DiceGame/DiceGame/Dice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigDiceGame
+{
+    public class Dice
+    {
+        int valueShown;
+        int numberOfSides;
+
+        public Dice(int numberOfSides)
+        {
+            this.valueShown = 0;
+            this.numberOfSides = numberOfSides;
+        }
+
+
+       public int getDiceRoll(int numberOfSides)
+        {
+            Random random = new Random();
+            this.valueShown = random.Next(1, (numberOfSides + 1));
+            return this.valueShown;
+        }
+    }
+}
